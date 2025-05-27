1c737729a1998d4fa0699675b32cd2fffd54dc45
Lucas Walter <wsacul@gmail.com>
2025-05-27 11:31:55 -0700

    convert ros1 mcap CompressedImage to h264

--- build.rs
+++ build.rs
@@ -1,10 +1,1 @@
-use protobuf_codegen::Codegen;
-
-fn main() {
-    Codegen::new()
-        .pure()
-        .cargo_out_dir("generated_protos")
-        .include("src/protos")
-        .input("src/protos/CompressedVideo.proto")
-        .run_from_script();
-}
+fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -4,93 +4,86 @@ use image::io::Reader as ImageReader;
 use memmap::Mmap;
 use openh264::encoder::{Encoder, EncoderConfig};
 use openh264::formats::YUVBuffer;
-use protobuf::descriptor::FileDescriptorSet;
-use protobuf::reflect::FileDescriptor;
-use protobuf::Message;
-use std::borrow::Cow;
-use std::collections::HashMap;
+use roslibrust::RosMessageType;
+use roslibrust_util::{sensor_msgs::CompressedImage, std_msgs::Header};
+use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
 use std::io::BufWriter;
 use std::io::Cursor;
-use std::sync::Arc;
 use std::{env, fs};
-
-mod foxglove {
-    include!(concat!(env!("OUT_DIR"), "/generated_protos/mod.rs"));
-}
 
 fn map_mcap<P: AsRef<Utf8Path>>(p: P) -> Result<Mmap> {
     let fd = fs::File::open(p.as_ref()).context("Couldn't open MCAP file")?;
     unsafe { Mmap::map(&fd) }.context("Couldn't map MCAP file")
 }
 
-fn read_it() -> Result<()> {
+// TODO(lucasw) make a mcap_tools utility function for this
+fn mcap_write<T: RosMessageType>(
+    mcap_out: &mut mcap::Writer<BufWriter<File>>,
+    msg: &T,
+    header: Header,
+    channel_id: u16,
+) -> Result<(), mcap::McapError> {
+    let sequence = 0;
+    let data = serde_rosmsg::to_vec(&msg).unwrap();
+    let log_time = tf_roslibrust::tf_util::stamp_to_duration(&header.stamp)
+        .num_nanoseconds()
+        .unwrap() as u64;
+    mcap_out.write_to_known_channel(
+        &mcap::records::MessageHeader {
+            channel_id,
+            sequence,
+            log_time,
+            publish_time: log_time,
+        },
+        &data[4..], // chop off header bytes
+    )
+}
+
+fn read_it() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
 
     let mapped = map_mcap(&args[1])?;
 
-    let mut set = FileDescriptorSet::new();
-    set.file
-        .push(foxglove::CompressedVideo::file_descriptor().proto().clone());
-    set.file.push(
-        ::protobuf::well_known_types::timestamp::file_descriptor()
-            .proto()
-            .clone(),
-    );
-
-    let cow = Cow::from(set.write_to_bytes().unwrap());
-
-    let compressed_video_schema = mcap::Schema {
-        name: "foxglove.CompressedVideo".to_string(),
-        encoding: "protobuf".to_string(),
-        data: cow.clone(),
-    };
-
-    // Map of topic -> channel for the topic
-    let mut topic_channels: HashMap<String, mcap::Channel> = HashMap::new();
-
     let mut encoders_by_topic: HashMap<String, Encoder> = HashMap::new();
 
+    // Map of topic -> channel for the topic
+    let mut topic_channel_ids: HashMap<String, u16> = HashMap::new();
+
+    // TODO(lucasw) disable zstd compression, it isn't going to do anything
     let mut video_mcap = mcap::Writer::new(BufWriter::new(
         File::create("compressed_video.mcap").unwrap(),
     ))
     .unwrap();
 
+    let schema_id = video_mcap.add_schema(
+        CompressedImage::ROS_TYPE_NAME,
+        "ros1msg",
+        CompressedImage::DEFINITION.as_bytes(),
+    )?;
+
+    let mut connection_summary = BTreeMap::new();
+
+    let mut count = 0;
     for message in mcap::MessageStream::new(&mapped)? {
         let full_message = message.unwrap();
         let schema = full_message.channel.schema.as_ref().unwrap().clone();
-
-        if schema.name.ne("foxglove.CompressedImage") || schema.encoding.ne("protobuf") {
+        if schema.name.ne("sensor_msgs/CompressedImage") {
             continue;
         }
 
-        let set_proto = FileDescriptorSet::parse_from_bytes(&schema.data)?;
-        let descriptors = FileDescriptor::new_dynamic_fds(set_proto.file, &[]).unwrap();
+        count += 1;
+        println!("{count}");
 
-        // fixme - why index 1?
-        let msg = descriptors[1]
-            .message_by_full_name(".foxglove.CompressedImage")
-            .unwrap();
+        let msg_with_header = roslibrust_util::get_message_data_with_header(full_message.data);
+        let msg = serde_rosmsg::from_slice::<CompressedImage>(&msg_with_header).unwrap();
 
-        let parsed = msg.parse_from_bytes(&full_message.data)?;
+        // println!("{:?}", msg);
+        let timestamp = msg.header.stamp;
+        let frame_id = msg.header.frame_id;
+        let data = msg.data;
 
-        println!("{:?}", msg);
-        let timestamp = msg
-            .field_by_name("timestamp")
-            .unwrap()
-            .get_singular_field_or_default(parsed.as_ref());
-
-        let frame_id = msg
-            .field_by_name("frame_id")
-            .unwrap()
-            .get_singular_field_or_default(parsed.as_ref());
-
-        let data = msg
-            .field_by_name("data")
-            .unwrap()
-            .get_singular_field_or_default(parsed.as_ref());
-
-        let reader = ImageReader::new(Cursor::new(data.to_bytes().unwrap()))
+        let reader = ImageReader::new(Cursor::new(data))
             .with_guessed_format()
             .expect("Cursor io never fails");
 
@@ -103,62 +96,37 @@ fn read_it() -> Result<()> {
 
         let topic = std::format!("{topic}_video", topic = full_message.channel.topic);
 
-        let encoder = encoders_by_topic.entry(topic.clone()).or_insert_with(||{
+        let encoder = encoders_by_topic.entry(topic.clone()).or_insert_with(|| {
             // fixme - command line argument for bitrate
             let config =
                 EncoderConfig::new(rgb8.width(), rgb8.height()).set_bitrate_bps(10_000_000);
-            return Encoder::with_config(config).unwrap();
+            Encoder::with_config(config).unwrap()
         });
-         
-        let yuv = YUVBuffer::with_rgb(width, height, &rgb8);
+
+        let yuv = YUVBuffer::with_rgb(width, height, rgb8);
         let bitstream = encoder.encode(&yuv).unwrap();
 
-        let mut out_msg = foxglove::CompressedVideo::CompressedVideo::new();
-
-        let bytes = timestamp
-            .to_message()
-            .unwrap()
-            .write_to_bytes_dyn()
-            .unwrap();
-        let time =
-            protobuf::well_known_types::timestamp::Timestamp::parse_from_bytes(bytes.as_slice())
-                .unwrap();
-        out_msg.timestamp.mut_or_insert_default().seconds = time.seconds;
-        out_msg.timestamp.mut_or_insert_default().nanos = time.nanos;
-
-        out_msg.frame_id = frame_id.to_string();
+        let mut out_msg = CompressedImage::default();
+        out_msg.header.stamp = timestamp;
+        out_msg.header.frame_id = frame_id;
         out_msg.format = "h264".to_string();
         out_msg.data = bitstream.to_vec();
 
-        let out_bytes: Vec<u8> = out_msg.write_to_bytes().unwrap();
+        let channel_id = topic_channel_ids
+            .entry(topic.clone())
+            .or_insert_with_key(|topic| {
+                connection_summary.insert("topic".to_string(), topic.to_string());
+                video_mcap
+                    .add_channel(schema_id, topic, "ros1", &connection_summary)
+                    .unwrap()
+            });
 
-        let channel = topic_channels.entry(topic.clone()).or_insert_with_key(|key| {
-            let new_channel = mcap::Channel {
-                schema: Some(Arc::new(compressed_video_schema.to_owned())),
-                topic: key.to_string(),
-                message_encoding: "protobuf".to_string(),
-                metadata: std::collections::BTreeMap::new(),
-            };
-
-            video_mcap
-                .add_channel(&new_channel)
-                .expect("Couldn't write channel");
-
-            return new_channel;
-        });
-
-        let message = mcap::Message {
-            channel: Arc::new(channel.to_owned()),
-            data: Cow::from(out_bytes),
-            log_time: full_message.log_time,
-            publish_time: full_message.publish_time,
-            sequence: full_message.sequence,
-        };
-
-        // fixme - why would out_bytes be 0? if the frame did not change?
-        if out_msg.data.len() > 0 {
-            video_mcap.write(&message).unwrap();
-        }
+        mcap_write::<CompressedImage>(
+            &mut video_mcap,
+            &out_msg,
+            out_msg.header.clone(),
+            *channel_id,
+        )?;
     }
 
     video_mcap.finish().unwrap();
